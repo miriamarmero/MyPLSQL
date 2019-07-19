2cb0b8edc374971ec168e0a31b711f867f79df8c
Miriam <norma.armero@gmail.com>
2019-07-19 10:02:43 +0200

    añadido de apuntes de excepciones

---
+++ GetNameById.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE FUNCTION GET_NAME
+(ID NUMBER)
+RETURN VARCHAR AS
+    name VARCHAR2(20);
+BEGIN
+    select CUST_FIRST_NAME INTO NAME
+    FROM DEMO_CUSTOMERS
+    WHERE CUSTOMER_ID = id;
+    
+    select ORDER_ID INTO NAME
+    FROM DEMO_ORDERS
+    WHERE ORDER_ID = id;
+    
+    return name;
+EXCEPTION
+    WHEN no_data_found then
+        return '';
+END;
+/
+
+DECLARE
+    nombre varchar2(20); 
+    NO_CUSTOMER_FOUND exception;
+BEGIN
+    nombre := GET_NAME(111);
+
+    IF LENGTH(nombre) IS NULL THEN
+        DBMS_OUTPUT.PUT_LINE('exception: ');
+        RAISE NO_CUSTOMER_FOUND;
+    END IF;
+    
+    DBMS_OUTPUT.PUT_LINE('Nombre: ' || nombre);
+EXCEPTION
+    WHEN NO_CUSTOMER_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('No encontré ese customer ');
+        --INSERT INTO DEMO_LOGS VALUES ('Customer no encontrado');
+END;
